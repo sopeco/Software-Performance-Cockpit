58a15e0bea447facff8275f042ff37f5ca204395
Roozbeh Farahbod <roozbeh.farahbod@sap.com>
2012-04-19 17:23:40 +0200

    Integrated EngineFactory. Added a simple test for FullExploration.
    Change-Id: Iaacde9903571269d0e46c90db07f4ec3863a06f6
    Signed-off-by: Roozbeh Farahbod <roozbeh.farahbod@sap.com>

--- org.sopeco.engine/src/org/sopeco/engine/EngineFactory.java
+++ org.sopeco.engine/src/org/sopeco/engine/EngineFactory.java
@@ -5,10 +5,33 @@ import java.net.URI;
 import org.sopeco.engine.experiment.IExperimentController;
 import org.sopeco.engine.experiment.impl.ExperimentController;
 import org.sopeco.engine.measurementenvironment.IMeasurementEnvironmentController;
+import org.sopeco.model.configuration.ScenarioDefinition;
 
 public class EngineFactory {
 
 	public static EngineFactory INSTANCE = new EngineFactory();
+
+	protected static IEngine engine = null;
+	
+	/**
+	 * Creates an engine based on the given scenario definition. 
+	 * 
+	 * @param scenarioDefinition a scenario definition
+	 */
+	public IEngine createEngine(ScenarioDefinition scenarioDefinition) {
+		
+		// TODO properly create an engine
+		
+		engine = new EngineImp();
+		return engine;
+	}
+	
+	/**
+	 * Returns the last created engine
+	 */
+	public IEngine getEngine() {
+		return engine;
+	}
 
 	public IExperimentController createExperimentController(IMeasurementEnvironmentController meController) {
 		ExperimentController expController = new ExperimentController();
--- org.sopeco.plugin.std.exploration/test/org/sopeco/plugin/std/exploration/FullExplorationStrategyTest.java
+++ org.sopeco.plugin.std.exploration/test/org/sopeco/plugin/std/exploration/FullExplorationStrategyTest.java
@@ -2,24 +2,56 @@ package org.sopeco.plugin.std.exploration;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.sopeco.engine.EngineFactory;
+import org.sopeco.engine.experiment.IExperimentController;
 import org.sopeco.engine.experimentseries.IExplorationStrategy;
+import org.sopeco.engine.helper.ConfigurationBuilder;
+import org.sopeco.engine.helper.DummyMEController;
+import org.sopeco.model.configuration.measurements.ExperimentSeriesDefinition;
+import org.sopeco.model.configuration.measurements.ExplorationStrategy;
+import org.sopeco.model.configuration.measurements.MeasurementsFactory;
 
 public class FullExplorationStrategyTest {
 
 	IExplorationStrategy fes = null;
 	
+	private DummyMEController meController;
+	private IExperimentController expController;
+	private ExperimentSeriesDefinition expSeriesDef; 
+	private ConfigurationBuilder builder;
+	
 	@Before
 	public void setUp() throws Exception {
+		builder = new ConfigurationBuilder("test");
+
 		FullExplorationStrategyExtension fese = new FullExplorationStrategyExtension();
 		fes = fese.createExtensionArtifact();
+
+		meController = new DummyMEController();
+		expController = EngineFactory.INSTANCE.createExperimentController(meController);
+		
+		expSeriesDef = MeasurementsFactory.eINSTANCE.createExperimentSeriesDefinition();
+		builder.createNumberOfRunsCondition(2);
+		expSeriesDef.setExperimentTerminationCondition(builder.getTerminationCondition());
+		
+		ExplorationStrategy es = MeasurementsFactory.eINSTANCE.createExplorationStrategy();
+		es.setName(FullExplorationStrategyExtension.NAME);
+		expSeriesDef.setExplorationStrategy(es);
+		
+		expSeriesDef.setName("experiment series");
+		
 	}
 
 	@Test
 	public void testCanRun() {
+		fes.canRun(expSeriesDef);
 	}
 
-	@Test
-	public void testRunExperimentSeries() {
-	}
+	// TODO test runExperimentSeries (requires parameter variation)
+	
+//	@Test
+//	public void testRunExperimentSeries() {
+//		fes.runExperimentSeries(expSeriesDef, parameterVariations, builder.getTerminationCondition());
+//	}
 
 }
