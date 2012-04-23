18a1f8ec97a20a91f94b771f66ab04f60fdcbcaf
Dennis Westermann <dennis.westermann@sap.com>
2012-04-23 13:01:23 +0200

    implemented MEConnector
    Change-Id: Id9b394d31aa9e533a9e0a7f128581185e06c5293
    Signed-off-by: Dennis Westermann <dennis.westermann@sap.com>

--- org.sopeco.engine/src/org/sopeco/engine/experiment/impl/MEConnector.java
+++ org.sopeco.engine/src/org/sopeco/engine/experiment/impl/MEConnector.java
@@ -1,5 +1,50 @@
 package org.sopeco.engine.experiment.impl;
 
-public class MEConnector {
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RMISecurityManager;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sopeco.engine.measurementenvironment.IMeasurementEnvironmentController;
+
+public class MEConnector {
+	private static Logger logger = LoggerFactory.getLogger(MEConnector.class);
+	
+	public static IMeasurementEnvironmentController getMeasurementEnvironmentController(URI uri){
+
+		// set path to java policy file TODO: Read path from Config-File
+		System.setProperty("java.security.policy", "rsc/wideopen.policy");
+	
+		if (System.getSecurityManager() == null)
+       		System.setSecurityManager ( new RMISecurityManager() );
+    
+		try {
+			  	LocateRegistry.getRegistry("localhost", 1099);
+				
+				logger.debug("Looking up {}", uri.toString());
+				
+				IMeasurementEnvironmentController meController = 
+				(IMeasurementEnvironmentController) Naming.lookup(uri.toString());
+						
+				logger.info("Received SatelliteController instance from {}", uri.toString());
+				
+				return meController;
+		    
+	    } catch (RemoteException e) {
+	    	
+			throw new IllegalStateException("Cannot access remote controller.", e);
+		} catch (MalformedURLException e) {
+			
+			throw new IllegalStateException("Malformed URL.", e);
+		} catch (NotBoundException e) {
+		
+			throw new IllegalStateException("NotBoundException: ", e);
+		} 
+		
+	}
 }
