e5ebf376cc43d54a446c99b8551368a2d3d474d6
Roozbeh Farahbod <roozbeh.farahbod@sap.com>
2012-05-07 10:09:43 +0200

    minor changes to the test file
    Change-Id: I7fc324c2d7af1741c616bfe005e3fdfdfed0c5c5
    Signed-off-by: Roozbeh Farahbod <roozbeh.farahbod@sap.com>

--- org.sopeco.core.test/src/org/sopeco/core/test/SoPeCoExecutableTest.java
+++ org.sopeco.core.test/src/org/sopeco/core/test/SoPeCoExecutableTest.java
@@ -15,7 +15,6 @@ import org.sopeco.persistence.PersistenceProviderFactory;
 import org.sopeco.persistence.dataset.DataSetAggregated;
 import org.sopeco.persistence.dataset.DataSetInputColumn;
 import org.sopeco.persistence.entities.ScenarioInstance;
-import org.sopeco.util.Tools;
 
 /**
  * This is a test for running SoPeCo as a simple Java application.
@@ -51,14 +50,12 @@ public class SoPeCoExecutableTest {
 		
 		DataSetAggregated data = si.getExperimentSeriesList().get(0).getExperimentSeriesRuns().get(0).getResultDataSet();
 		
-		System.out.println(data.getInputColumns());
+		logger.debug("Logging the result of the first experiement series run:");
 		
 		for (DataSetInputColumn<?> ic: data.getInputColumns()) {
-			System.out.println(ic.getValueList());
+			logger.debug("{}: {}", ic.getParameter().getName(), ic.getValueList());
 		}
 
-		System.out.println(runner.getScenarioInstance().getName());
-		
 		IPersistenceProvider pp = PersistenceProviderFactory.getPersistenceProvider();
 	}
 	
