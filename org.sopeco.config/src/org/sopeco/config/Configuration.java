2009a9dd8b1640de21ce1ae7a193f3d1a5edb935
Jens Happe <jens.happe@sap.com>
2012-07-25 13:30:07 +0200

    Fixed OS error when loading resources from the classpath.
    Change-Id: Ia13b943a6787f6bfafecc4f00596f1ec823b2079
    Signed-off-by: Jens Happe <jens.happe@sap.com>

--- org.sopeco.config/src/org/sopeco/config/Configuration.java
+++ org.sopeco.config/src/org/sopeco/config/Configuration.java
@@ -31,12 +31,11 @@ import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.core.joran.spi.JoranException;
 
-
 /**
- * This class is the access point to all SoPeCo global configuration. 
+ * This class is the access point to all SoPeCo global configuration.
  * 
  * @author Roozbeh Farahbod
- *
+ * 
  */
 public class Configuration implements IConfiguration {
 
@@ -47,7 +46,7 @@ public class Configuration implements IConfiguration {
 
 	/** Holds the default property values. */
 	private Map<String, Object> defaultValues = new HashMap<String, Object>();
-	
+
 	/** Holds the configured property values. */
 	private Map<String, Object> properties = new HashMap<String, Object>();
 
@@ -56,34 +55,35 @@ public class Configuration implements IConfiguration {
 	 */
 	private Configuration(Class<?> mainClass) {
 		try {
-			logger.info("Initializing SoPeCo configuration module{}.", (mainClass==null?"":" with main class " + mainClass.getName() + ""));
+			logger.info("Initializing SoPeCo configuration module{}.", (mainClass == null ? "" : " with main class " + mainClass.getName() + ""));
 			setDefaultValues(mainClass);
 		} catch (ConfigurationException e) {
 			throw new RuntimeException(e);
 		}
 	};
-	
+
 	/**
 	 * Returns a singleton instance of the kernel.
+	 * 
 	 * @return
 	 */
 	public static IConfiguration getSingleton() {
 		if (SINGLETON == null)
 			SINGLETON = new Configuration(null);
-		
+
 		return SINGLETON;
 	}
 
 	/**
-	 * Returns a singleton instance of the configuration with 
-	 * the given main class.
+	 * Returns a singleton instance of the configuration with the given main
+	 * class.
 	 * 
 	 * @see #setMainClass(Class)
 	 */
 	public static IConfiguration getSingleton(Class<?> mainClass) {
 		if (SINGLETON == null)
 			SINGLETON = new Configuration(mainClass);
-		
+
 		return SINGLETON;
 	}
 
@@ -94,7 +94,7 @@ public class Configuration implements IConfiguration {
 			value = System.getProperty(key);
 		if (value == null)
 			value = defaultValues.get(key);
-		
+
 		return value;
 	}
 
@@ -116,8 +116,8 @@ public class Configuration implements IConfiguration {
 			return Tools.strEqualName("true", value) || Tools.strEqualName("yes", value);
 	}
 
-	@Override 
-	public long getPropertyAsLong(String key, long defaultValue){
+	@Override
+	public long getPropertyAsLong(String key, long defaultValue) {
 		final String value = getPropertyAsStr(key);
 		if (value == null) {
 			return defaultValue;
@@ -127,7 +127,7 @@ public class Configuration implements IConfiguration {
 	}
 
 	@Override
-	public int getPropertyAsInteger(String key, int defaultValue){
+	public int getPropertyAsInteger(String key, int defaultValue) {
 		final String value = getPropertyAsStr(key);
 		if (value == null) {
 			return defaultValue;
@@ -136,7 +136,6 @@ public class Configuration implements IConfiguration {
 		}
 	}
 
-	
 	@Override
 	public void setProperty(String key, Object value) {
 		properties.put(key, value);
@@ -150,140 +149,119 @@ public class Configuration implements IConfiguration {
 	@Override
 	public void processCommandLineArguments(String[] args) throws ConfigurationException {
 		Options options = new Options();
-		
+
 		// option: help
-		Option help = new Option( "help", "print this message" );
-		
+		Option help = new Option("help", "print this message");
+
 		// option: sopeco config file
-		Option config = OptionBuilder.withArgName("file")
-										.hasArg()
-										.withDescription("sopeco configuration file")
-										.create("config");
+		Option config = OptionBuilder.withArgName("file").hasArg().withDescription("sopeco configuration file").create("config");
 
 		// option: logger config file
-		Option logconfig = OptionBuilder.withArgName("file")
-										.hasArg()
-										.withDescription("the logback configuration file")
-										.create("logconfig");
+		Option logconfig = OptionBuilder.withArgName("file").hasArg().withDescription("the logback configuration file").create("logconfig");
 
 		// option: ME URI
-		Option meURI = OptionBuilder.withArgName("URI")
-										.hasArg()
-										.withDescription("URI of the measurement environment controller service")
-										.create("uri");
-		
-		// option: ME Class
-		Option meClass = OptionBuilder.withArgName("class")
-										.hasArg()
-										.withDescription("classname of the measurement environment controller")
-										.create("meClass");
-		
-		// option: scenario definition
-		Option scenarioDef = OptionBuilder.withArgName("file")
-										.hasArg()
-										.withDescription("scenario definition file")
-										.create("sd");
-		Option modelChangeHandling = OptionBuilder.withArgName("mchMode")
-				.hasArg()
-				.withDescription("mode describing how to handle model changes")
-				.create("mch");
+		Option meURI = OptionBuilder.withArgName("URI").hasArg().withDescription("URI of the measurement environment controller service").create("uri");
 
-		// option 
-		Option logVerbosity = OptionBuilder.withArgName("level")
-				.hasArg()
-				.withDescription("logging verbosity level (overrides log config)")
-				.create("lv");
-		
-//		Option selectedSeries = OptionBuilder.withArgName("series")
-//		.withValueSeparator(',')
-//		.hasArg()
-//		.withDescription("the names of selected experiment series separated by comma")
-//		.create("series");
+		// option: ME Class
+		Option meClass = OptionBuilder.withArgName("class").hasArg().withDescription("classname of the measurement environment controller").create("meClass");
+
+		// option: scenario definition
+		Option scenarioDef = OptionBuilder.withArgName("file").hasArg().withDescription("scenario definition file").create("sd");
+		Option modelChangeHandling = OptionBuilder.withArgName("mchMode").hasArg().withDescription("mode describing how to handle model changes").create("mch");
+
+		// option
+		Option logVerbosity = OptionBuilder.withArgName("level").hasArg().withDescription("logging verbosity level (overrides log config)").create("lv");
+
+		// Option selectedSeries = OptionBuilder.withArgName("series")
+		// .withValueSeparator(',')
+		// .hasArg()
+		// .withDescription("the names of selected experiment series separated by comma")
+		// .create("series");
 
 		OptionGroup meGroup = new OptionGroup();
 		meGroup.addOption(meURI);
 		meGroup.addOption(meClass);
-		
+
 		options.addOption(scenarioDef);
 		options.addOption(help);
 		options.addOption(config);
 		options.addOptionGroup(meGroup);
 		options.addOption(logconfig);
 		options.addOption(modelChangeHandling);
-//		options.addOption(logVerbosity);
+		// options.addOption(logVerbosity);
 
-	    CommandLineParser parser = new GnuParser();
-	    CommandLine line = null;
-	    try {
-	        line = parser.parse( options, args );
-	    }
-	    catch( ParseException exp ) {
-	    	if (Tools.exists("-help", args) < 0) {
-		    	final String err = "Invalid options. " + exp.getMessage();
-		        System.err.println(err);
-		        printHelpMessage(options);
-		        throw new ConfigurationException(err, exp);
-	    	} else {
-	    		printHelpMessage(options);
-	    		System.exit(0);
-	    	}
-	    }
+		CommandLineParser parser = new GnuParser();
+		CommandLine line = null;
+		try {
+			line = parser.parse(options, args);
+		} catch (ParseException exp) {
+			if (Tools.exists("-help", args) < 0) {
+				final String err = "Invalid options. " + exp.getMessage();
+				System.err.println(err);
+				printHelpMessage(options);
+				throw new ConfigurationException(err, exp);
+			} else {
+				printHelpMessage(options);
+				System.exit(0);
+			}
+		}
 
-	    // -help
-	    if (line.hasOption(help.getOpt())) {
-	        printHelpMessage(options);
-	        System.exit(0);
-	    }
+		// -help
+		if (line.hasOption(help.getOpt())) {
+			printHelpMessage(options);
+			System.exit(0);
+		}
 
-	    if (line.hasOption(modelChangeHandling.getOpt())) {
-	    	final String mchMode = line.getOptionValue(modelChangeHandling.getOpt());
-	    	setProperty(CONF_MODEL_CHANGE_HANDLING_MODE, mchMode);
-	    }
-	    
-	    // -sd 
-	    setScenarioDescriptionFileName(line.getOptionValue(scenarioDef.getOpt()));
-	    
-	    // -config
-	    if (line.hasOption(config.getOpt())) {
-	    	final String configFilePath = line.getOptionValue(config.getOpt());
-	    	loadConfiguration(configFilePath);
-	    }
-	    
-	    // -uri
-	    if (line.hasOption(meURI.getOpt())) {
-	    	final String uriStr = line.getOptionValue(meURI.getOpt());
-	    	setMeasurementControllerURI(uriStr);
-	    }
-	    
-	    // -meClass
-	    if (line.hasOption(meClass.getOpt())) {
-	    	final String className = line.getOptionValue(meClass.getOpt());
-	    	setMeasurementControllerClassName(className);
-	    }
+		if (line.hasOption(modelChangeHandling.getOpt())) {
+			final String mchMode = line.getOptionValue(modelChangeHandling.getOpt());
+			setProperty(CONF_MODEL_CHANGE_HANDLING_MODE, mchMode);
+		}
 
-	    // -logconfig
-	    if (line.hasOption(logconfig.getOpt())) {
-	    	setLoggerConfigFileName(line.getOptionValue(logconfig.getOpt()));
-	    }
-	    
-	    // -lv
-	    if (line.hasOption(logVerbosity.getOpt())) {
-	    	final String verbosity = line.getOptionValue(logVerbosity.getOpt());
-	    	// TODO it may not be possible
-	    }
+		// -sd
+		setScenarioDescriptionFileName(line.getOptionValue(scenarioDef.getOpt()));
 
-	} 
+		// -config
+		if (line.hasOption(config.getOpt())) {
+			final String configFilePath = line.getOptionValue(config.getOpt());
+			loadConfiguration(configFilePath);
+		}
+
+		// -uri
+		if (line.hasOption(meURI.getOpt())) {
+			final String uriStr = line.getOptionValue(meURI.getOpt());
+			setMeasurementControllerURI(uriStr);
+		}
+
+		// -meClass
+		if (line.hasOption(meClass.getOpt())) {
+			final String className = line.getOptionValue(meClass.getOpt());
+			setMeasurementControllerClassName(className);
+		}
+
+		// -logconfig
+		if (line.hasOption(logconfig.getOpt())) {
+			setLoggerConfigFileName(line.getOptionValue(logconfig.getOpt()));
+		}
+
+		// -lv
+		if (line.hasOption(logVerbosity.getOpt())) {
+			final String verbosity = line.getOptionValue(logVerbosity.getOpt());
+			// TODO it may not be possible
+		}
+
+	}
 
 	/**
-	 * Sets the default property values. 
-	 * @throws ConfigurationException 
+	 * Sets the default property values.
+	 * 
+	 * @throws ConfigurationException
 	 */
 	private void setDefaultValues(Class<?> mainClass) throws ConfigurationException {
 		if (mainClass != null)
 			defaultValues.put(CONF_MAIN_CLASS, mainClass);
-		
+
 		if (getAppRootDirectory() == null)
-		defaultValues.put(CONF_APP_ROOT_FOLDER, Tools.getRootFolder());
+			defaultValues.put(CONF_APP_ROOT_FOLDER, Tools.getRootFolder());
 
 		loadDefaultConfiguration(DEFAULT_CONFIG_FILE_NAME);
 	}
@@ -291,13 +269,14 @@ public class Configuration implements IConfiguration {
 	/**
 	 * Prints a usage help message.
 	 * 
-	 * @param options command-line options 
+	 * @param options
+	 *            command-line options
 	 */
 	private void printHelpMessage(Options options) {
 		HelpFormatter formatter = new HelpFormatter();
 		formatter.setWidth(120);
 		formatter.setArgName("Test");
-		formatter.printHelp(getApplicationName(), options, true );
+		formatter.printHelp(getApplicationName(), options, true);
 	}
 
 	@Override
@@ -353,9 +332,9 @@ public class Configuration implements IConfiguration {
 
 	@Override
 	public URI getMeasurementControllerURI() {
-		return (URI)getProperty(CONF_MEASUREMENT_CONTROLLER_URI);
+		return (URI) getProperty(CONF_MEASUREMENT_CONTROLLER_URI);
 	}
-	
+
 	@Override
 	public String getMeasurementControllerClassName() {
 		return getPropertyAsStr(CONF_MEASUREMENT_CONTROLLER_CLASS_NAME);
@@ -365,7 +344,7 @@ public class Configuration implements IConfiguration {
 	public String getApplicationName() {
 		return getPropertyAsStr(CONF_APP_NAME);
 	}
-	
+
 	@Override
 	public String getAppRootDirectory() {
 		String result = getPropertyAsStr(CONF_APP_ROOT_FOLDER);
@@ -383,7 +362,7 @@ public class Configuration implements IConfiguration {
 	public String getAppConfDirectory() {
 		return getAppRootDirectory() + File.separator + DEFAULT_CONFIG_FOLDER_NAME;
 	}
-	
+
 	public void setMainClass(Class<?> mainClass) {
 		setProperty(CONF_MAIN_CLASS, mainClass);
 	}
@@ -395,20 +374,22 @@ public class Configuration implements IConfiguration {
 
 	@Override
 	public void setLoggerConfigFileName(String fileName) {
-        setProperty(CONF_LOGGER_CONFIG_FILE_NAME, fileName);
-        configureLogger();
+		setProperty(CONF_LOGGER_CONFIG_FILE_NAME, fileName);
+		configureLogger();
 	}
 
 	/**
-	 * Configures the logging system with the provided logger configuration file.
+	 * Configures the logging system with the provided logger configuration
+	 * file.
 	 */
 	private void configureLogger() {
-    	// The following code loads the logback config file using JoranConfigurator.
-    	// Alternatively, you can specify the location of the config file using
-    	// the system property 'logback.configurationFile'
-    	// e.g., 
-    	// $ java -Dlogback.configurationFile=/path/to/config.xml ...
-        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+		// The following code loads the logback config file using
+		// JoranConfigurator.
+		// Alternatively, you can specify the location of the config file using
+		// the system property 'logback.configurationFile'
+		// e.g.,
+		// $ java -Dlogback.configurationFile=/path/to/config.xml ...
+		LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
 
 		String fileName = getPropertyAsStr(CONF_LOGGER_CONFIG_FILE_NAME);
 		if (fileName != null) {
@@ -432,33 +413,31 @@ public class Configuration implements IConfiguration {
 			logger.debug("Logback configured.");
 		}
 	}
-	
+
 	@Override
 	public void writeConfiguration(String fileName) throws IOException {
 		Properties props = new Properties();
-		
+
 		logger.debug("Writing configuration file to {}...", fileName);
-		
-		for (Entry<String, Object> e: defaultValues.entrySet()) 
+
+		for (Entry<String, Object> e : defaultValues.entrySet())
 			copyConfigItem(e, props);
-		
-		for (Entry<String, Object> e: properties.entrySet()) 
+
+		for (Entry<String, Object> e : properties.entrySet())
 			copyConfigItem(e, props);
-		
+
 		props.store(new FileOutputStream(fileName), "SoPeCo Configuration");
-	
+
 		logger.debug("Configuration file written to {}.", fileName);
 	}
 
 	private void copyConfigItem(Entry<String, Object> e, Properties destination) {
-		if (e.getValue() instanceof Number
-				|| e.getValue() instanceof Boolean
-				|| e.getValue() instanceof String) {
+		if (e.getValue() instanceof Number || e.getValue() instanceof Boolean || e.getValue() instanceof String) {
 			destination.setProperty(e.getKey(), e.getValue().toString());
 		} else
 			logger.debug("Skipping configuration item '{}'.", e.getKey());
 	}
-	
+
 	@Override
 	public void loadDefaultConfiguration(String fileName) throws ConfigurationException {
 		loadConfiguration(defaultValues, ClassLoader.getSystemClassLoader(), fileName);
@@ -486,8 +465,9 @@ public class Configuration implements IConfiguration {
 		InputStream in = null;
 		try {
 			in = findConfigFileAsInputStream(classLoader, DEFAULT_CONFIG_FOLDER_NAME, fileName);
-		} catch (FileNotFoundException e) {}
-		
+		} catch (FileNotFoundException e) {
+		}
+
 		if (in == null)
 			logger.warn("Cannot load configuration file '{}'.", fileName);
 		else {
@@ -500,7 +480,8 @@ public class Configuration implements IConfiguration {
 	 * Loads configuration from a config stream into the destination
 	 * configuration holder.
 	 * 
-	 * @param stream the input stream
+	 * @param stream
+	 *            the input stream
 	 */
 	private void loadConfigFromStream(Map<String, Object> dest, InputStream stream) throws ConfigurationException {
 		Properties prop = new Properties();
@@ -511,51 +492,56 @@ public class Configuration implements IConfiguration {
 		}
 		for (Entry<Object, Object> entry : prop.entrySet()) {
 			if (dest == properties) {
-				setProperty((String)entry.getKey(), entry.getValue());
+				setProperty((String) entry.getKey(), entry.getValue());
 			} else
-				dest.put((String)entry.getKey(), entry.getValue());
+				dest.put((String) entry.getKey(), entry.getValue());
 		}
 	}
-	
 
 	/**
-	 * Looks for the given file and returns an input stream view of the file if it is found.
-	 * If the file name is not an absolute path, it looks for the file in the following order:
+	 * Looks for the given file and returns an input stream view of the file if
+	 * it is found. If the file name is not an absolute path, it looks for the
+	 * file in the following order:
 	 * <ol>
 	 * <li>the container directory,</li>
 	 * <li>current folder,</li>
-	 * <li>the container directory in classpath,</li> 
+	 * <li>the container directory in classpath,</li>
 	 * <li>and finally the classpath.</li>
 	 * </ol>
-	 * where classpath is determined by the given class loader.  
+	 * where classpath is determined by the given class loader.
 	 * 
 	 * @throws FileNotFoundException
 	 */
 	private InputStream findConfigFileAsInputStream(ClassLoader classLoader, String container, String fileName) throws FileNotFoundException {
 
 		String pathToFile = fileName;
-		
+
 		// 0. Is the file name an absolute path?
 
 		if (Tools.isAbsolutePath(pathToFile)) {
 			return new FileInputStream(pathToFile);
 		}
-		
+
 		// 1. Try the container directory, if it exists
-		pathToFile = Tools.concatFileName(getAppRootDirectory(), ((container==null || container.length() == 0)?fileName:(Tools.concatFileName(container, fileName))));
+		pathToFile = Tools.concatFileName(getAppRootDirectory(),
+				((container == null || container.length() == 0) ? fileName : (Tools.concatFileName(container, fileName))));
 		if (Tools.fileExists(pathToFile)) {
 			return new FileInputStream(pathToFile);
 		}
-		
+
 		// 2. Try the root directory
 		pathToFile = Tools.concatFileName(getAppRootDirectory(), fileName);
 		if (Tools.fileExists(pathToFile)) {
 			return new FileInputStream(pathToFile);
 		}
-		
-		// 3. Try the container directory in the classpath
-		pathToFile = (container==null || container.length() == 0)?fileName:(container + "/" + fileName);
-		
+
+		// 3.1 Convert all File.separators (like '\') to slashes. Otherwise the
+		// resource may not be found in the classpath.
+		fileName = fileName.replace(File.separatorChar, '/');
+
+		// 3.2 Try the container directory in the classpath
+		pathToFile = (container == null || container.length() == 0) ? fileName : (container + "/" + fileName);
+
 		InputStream inStream = classLoader.getResourceAsStream(pathToFile);
 		if (inStream == null) {
 			if (container != null && container.length() > 0) {
@@ -566,7 +552,7 @@ public class Configuration implements IConfiguration {
 
 			}
 		}
-		
+
 		if (inStream != null)
 			return inStream;
 		else
@@ -575,14 +561,11 @@ public class Configuration implements IConfiguration {
 
 }
 
-
-
-
-//// -series
-//if (line.hasOption(selectedSeries.getOpt())) {
-//	final String[] selectedNames = line.getOptionValues(selectedSeries.getOpt());
-//	this.selectedSeries = new HashSet<String>();
-//	for (String sname: selectedNames)
-//		this.selectedSeries.add(sname);
-//}
+// // -series
+// if (line.hasOption(selectedSeries.getOpt())) {
+// final String[] selectedNames = line.getOptionValues(selectedSeries.getOpt());
+// this.selectedSeries = new HashSet<String>();
+// for (String sname: selectedNames)
+// this.selectedSeries.add(sname);
+// }
 
