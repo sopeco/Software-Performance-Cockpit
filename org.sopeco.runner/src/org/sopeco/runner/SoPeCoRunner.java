683b07525fefde435d43292653ec3cdba45d8576
Alexander Wert <alexander.wert@sap.com>
2012-09-26 11:22:03 +0200

    adopted SoPeCoRunner to the new session related configurations
    Change-Id: I2008e13e7818bf9720e1d3b64c7d5d7f3939e9e0
    Signed-off-by: Alexander Wert <alexander.wert@sap.com>

--- org.sopeco.runner/src/org/sopeco/runner/SoPeCoRunner.java
+++ org.sopeco.runner/src/org/sopeco/runner/SoPeCoRunner.java
@@ -19,43 +19,68 @@ import org.sopeco.persistence.entities.definition.ScenarioDefinition;
 import org.sopeco.util.Tools;
 
 /**
- * The main runner class of SoPeCo. 
+ * The main runner class of SoPeCo.
  * 
  * @author Roozbeh Farahbod
- *
+ * 
  */
 public class SoPeCoRunner implements Runnable {
 
 	protected static final Logger logger = LoggerFactory.getLogger(SoPeCoRunner.class);
-	
+
 	protected String[] args = null;
-	
+
 	protected ScenarioInstance lastExecutedScenarioInstance = null;
-	
+
+	private String sessionId;
+
 	/**
-	 * Runs SoPeCo with the given command-line arguments. 
+	 * Use this constructor with caution! This constructor creates an own
+	 * session id. Session related configurations made outside will not be
+	 * propagated to the created SoPeCoRunner object, as the created object uses
+	 * an own configuration with an own session id.
+	 */
+	public SoPeCoRunner() {
+		this.sessionId = UUID.randomUUID().toString();
+	}
+
+	/**
+	 * Use this constructor to provide a session id.
 	 * 
-	 * @param args command-line arguments
+	 * @param sessionId
+	 *            session id to be used for retrieving configuration properties
+	 *            for this runner and all it triggered subprocesses.
+	 */
+	public SoPeCoRunner(String sessionId) {
+		this.sessionId = sessionId;
+	}
+
+	/**
+	 * Runs SoPeCo with the given command-line arguments.
+	 * 
+	 * @param args
+	 *            command-line arguments
 	 * 
 	 * @see #setArguments(String[])
 	 * @see #run()
 	 */
 	public static void main(String[] args) {
 		final SoPeCoRunner runner = new SoPeCoRunner();
-		
+
 		runner.setArguments(args);
 		runner.run();
 	}
 
 	/**
-	 * Assuming that the required command-line arguments (if any) are set already, using {@link SoPeCoRunner#setArguments(String[]). 
+	 * 
+	 * Assuming that the required command-line arguments (if any) are set
+	 * already, using {@link SoPeCoRunner#setArguments(String[]).
 	 */
 	@Override
-	public void run() {
-		String sessionId = UUID.randomUUID().toString();
+	public synchronized void run() {
 		lastExecutedScenarioInstance = null;
 		IConfiguration config = Configuration.getSessionSingleton(sessionId);
-	
+
 		// if the user has set any command-line arguments
 		if (args != null) {
 			try {
@@ -64,28 +89,27 @@ public class SoPeCoRunner implements Runnable {
 				return;
 			}
 		}
-		
+
 		ScenarioDefinition scenario = null;
 
 		Object scenarioObj = config.getScenarioDescription();
-        if (scenarioObj == null) {
-            String fileName = config.getScenarioDescriptionFileName();
-           
-            // if the scenario definition is not set as an object,
-            // then a filename should have been given
-            if (fileName == null) {
-                   throw new RuntimeException(new ConfigurationException("Scenario definition is not provided."));
-            }
-           
-            try {
-                   if (!Tools.isAbsolutePath(fileName)) {
-                          fileName = Tools.concatFileName(config.getAppRootDirectory(), fileName);
-                   }
-                   scenario = (ScenarioDefinition) EMFUtil.loadFromFilePath(fileName);
-                  
-				
+		if (scenarioObj == null) {
+			String fileName = config.getScenarioDescriptionFileName();
+
+			// if the scenario definition is not set as an object,
+			// then a filename should have been given
+			if (fileName == null) {
+				throw new RuntimeException(new ConfigurationException("Scenario definition is not provided."));
+			}
+
+			try {
+				if (!Tools.isAbsolutePath(fileName)) {
+					fileName = Tools.concatFileName(config.getAppRootDirectory(), fileName);
+				}
+				scenario = (ScenarioDefinition) EMFUtil.loadFromFilePath(fileName);
+
 				logger.debug("Scenario definition file loaded.");
-				
+
 			} catch (IOException e) {
 				logger.error("Cannot load scenario definition file ({}). Reason: ({}) {}", fileName, e.getMessage());
 				logger.debug("IO Exception occured.", e);
@@ -95,38 +119,39 @@ public class SoPeCoRunner implements Runnable {
 			if (scenarioObj instanceof ScenarioDefinition)
 				scenario = (ScenarioDefinition) scenarioObj;
 			else {
-				final String msg = "Scenario definition object is not of class " + ScenarioDefinition.class.getName() + ".";
+				final String msg = "Scenario definition object is not of class " + ScenarioDefinition.class.getName()
+						+ ".";
 				// TODO do it properly
 				throw new RuntimeException(new ConfigurationException(msg));
 			}
-			
+
 			logger.debug("Scenario definition is passed as an object.");
 		}
-		
+
 		IEngine engine = EngineFactory.getInstance().createEngine(sessionId);
-		
+
 		lastExecutedScenarioInstance = engine.run(scenario);
 
 		logger.info("SoPeCo run finished.");
 	}
 
 	/**
-	 * Sets the command-line arguments before running SoPeCo. 
+	 * Sets the command-line arguments before running SoPeCo.
 	 * 
 	 * @param args
 	 */
 	public void setArguments(String[] args) {
 		this.args = args;
 	}
-	
+
 	/**
-	 * Returns the result of the last run of SoPeCo. 
-	 * If there was no successful run, this method returns <code>null</code>.
+	 * Returns the result of the last run of SoPeCo. If there was no successful
+	 * run, this method returns <code>null</code>.
 	 * 
-	 * @return result of last run, or <code>null</code> if there was no successful run.
+	 * @return result of last run, or <code>null</code> if there was no
+	 *         successful run.
 	 */
 	public ScenarioInstance getScenarioInstance() {
 		return lastExecutedScenarioInstance;
 	}
 }
-
